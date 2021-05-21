a11e0b0c5fe194a4e971f539007af691af0872c1
Медведев Олег <medvedev@cmsplanet.ru>
2021-05-21 16:33:22 +0300

    Добавил новый компонент через терминал - "ng g c form --skipTests" + Добавил стили из Google Fonts

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule
